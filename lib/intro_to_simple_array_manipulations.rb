3351aa089e4716aa9aa459bf36d698010d596cc8
Marcus Siegel <msiegel37@gmail.com>
2020-08-07 00:57:44 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -11,4 +11,13 @@ def using_uniq(array)
 end 
 
 def using_flatten(array)
-  
+  array.flatten 
+end 
+
+def using_delete(array, string)
+  array.delete(string)
+end 
+
+def using_delete_at(array, i)
+  array.delete_at(i) 
+end 
