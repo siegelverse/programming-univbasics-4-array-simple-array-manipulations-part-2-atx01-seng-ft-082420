47f62a8ba4ad8d136fd591ec9e512bbf12812a6a
Marcus Siegel <msiegel37@gmail.com>
2020-08-07 00:55:27 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,4 +3,12 @@ def using_concat(array, array2)
 end 
 
 def using_insert(array, element)
+  array.insert(element)
+end 
+
+def using_uniq(array)
+  array.uniq 
+end 
+
+def using_flatten(array)
   
